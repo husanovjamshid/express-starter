9916ec7f32d8da28468c05d5872061353cdcd5d7
husanovjamshid <husanovjamshid11@gmail.com>
2023-06-07 17:10:23 +0500

    add env file

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
 
-const app = express()
+const app = express();
+const PORT = process.env.PORT;
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
-
-app.listen(5000, () => console.log('server is running'))
+app.listen(PORT, () => console.log('server is running', PORT));
